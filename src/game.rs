4d7c32e6e6c3a42b73121a4472ecadf43d31bd76
nhyne <adamjohnson35@gmail.com>
2019-05-27 16:19:44 -0400

    gravity works, needed a density for it

--- src/game.rs
+++ src/game.rs
@@ -10,7 +10,7 @@ use piston_window::*;
 use nalgebra::{Isometry2, Vector2};
 use ncollide2d::shape::{Cuboid, ShapeHandle};
 use nphysics2d::material::{BasicMaterial, MaterialHandle};
-use nphysics2d::object::{BodyPartHandle, BodyStatus, ColliderDesc, RigidBodyDesc};
+use nphysics2d::object::{BodyPartHandle, ColliderDesc, RigidBodyDesc};
 use nphysics2d::world::World;
 
 use std::collections::HashSet;
@@ -26,7 +26,7 @@ pub struct Game {
 impl Game {
     pub fn new() -> Game {
         let mut world: World<f64> = World::new();
-        world.set_gravity(Vector2::y() * -9.81);
+        world.set_gravity(Vector2::y() * 90.81);
 
         let player = Game::init_player(&mut world, (50.0, 200.0));
         Game::init_ground(&mut world);
@@ -74,13 +74,13 @@ impl Game {
     ) -> player::Player {
         let player_shape = ShapeHandle::new(Cuboid::new(Vector2::new(7.5, 25.0)));
         let player_collider = ColliderDesc::new(player_shape)
+            .density(1.0)
             .material(MaterialHandle::new(BasicMaterial::new(0.0, 0.0)));
-        let player_rb_desc = RigidBodyDesc::new().collider(&player_collider);
+        let mut player_rb_desc = RigidBodyDesc::new()
+            .collider(&player_collider)
+            .position(Isometry2::translation(position.0, position.1));
 
-        let player_rigid_body = player_rb_desc
-            .position(Isometry2::translation(position.0, position.1))
-            .gravity_enabled(true)
-            .build(world);
+        let player_rigid_body = player_rb_desc.build(world);
         let player_handle = player_rigid_body.handle();
 
         player::Player::new(player_handle)
